ae302e2a71bb0a3c821818fe62a54b1e80be6363
han <chos9715@naver.com>
2023-07-21 15:51:41 +0900

    [Init] Initial Commit

---
+++ src/test/java/com/hamgame/hamgame/HamgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamgame.hamgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
