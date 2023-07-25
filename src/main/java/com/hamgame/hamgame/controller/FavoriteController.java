377d394db81c4872e3ac15ca26ff44c4d62ebb4c
han <chos9715@naver.com>
2023-07-26 02:43:42 +0900

    [Feat] 즐겨찾는 게임 CRUD

--- src/main/java/com/hamgame/hamgame/controller/FavoriteController.java
+++ src/main/java/com/hamgame/hamgame/controller/FavoriteController.java
@@ -1,13 +1,19 @@
 package com.hamgame.hamgame.controller;
 
-import java.util.List;
+import java.util.Set;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hamgame.hamgame.domain.Game;
+import com.hamgame.hamgame.dto.AddFavGamesReq;
+import com.hamgame.hamgame.dto.RemoveFavGamesReq;
+import com.hamgame.hamgame.dto.UpdateFavGamesReq;
+import com.hamgame.hamgame.service.FavoriteService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,12 +22,26 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class FavoriteController {
 
-	@GetMapping("")
-	public List<Game> getFavoriteGameList() {
-		return null;
+	private final FavoriteService favoriteService;
+
+	@GetMapping
+	public Set<Game> getFavoriteGameList() {
+		return favoriteService.getFavoriteGameList();
 	}
 
-	@PostMapping("")
-	public void addFavoriteGames() {
+	@PostMapping
+	public void addFavoriteGames(AddFavGamesReq requestDto) {
+		favoriteService.addGame(requestDto);
 	}
+
+	@PutMapping
+	public void updateFavoriteGames(UpdateFavGamesReq requestDto) {
+		favoriteService.updateGames(requestDto);
+	}
+
+	@DeleteMapping
+	public void removeGames(RemoveFavGamesReq requestDto) {
+		favoriteService.removeGame(requestDto);
+	}
+
 }
