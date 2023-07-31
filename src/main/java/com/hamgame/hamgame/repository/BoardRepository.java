07996c5b7b22bda09c8c1e48d959aeb3f375e3f0
han <chos9715@naver.com>
2023-07-31 16:02:51 +0900

    [Feat] 게시글 CRUD Error code 추가

--- src/main/java/com/hamgame/hamgame/repository/BoardRepository.java
+++ src/main/java/com/hamgame/hamgame/repository/BoardRepository.java
@@ -1,5 +1,7 @@
 package com.hamgame.hamgame.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +12,7 @@ import com.hamgame.hamgame.domain.type.BoardCategory;
 public interface BoardRepository extends JpaRepository<Board, Long> {
 	Page<Board> findByGame_GameId(Long gameId, Pageable pageable);
 
-	Board findByBoardIdAndGame_GameId(Long boardId, Long gameId);
+	Optional<Board> findByBoardIdAndGame_GameId(Long boardId, Long gameId);
 
 	Page<Board> findByBoardCategoryAndGame_GameId(BoardCategory boardCategory, Long gameId, Pageable pageable);
 }
