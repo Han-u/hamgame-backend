ec8c5c237d1c1dac35f94e8c4c4c7cf2e35c7312
han <chos9715@naver.com>
2023-07-31 17:18:20 +0900

    [Feat] 즐겨찾는 게임 User 정보 추가

--- src/main/java/com/hamgame/hamgame/service/GameNoticeService.java
+++ src/main/java/com/hamgame/hamgame/service/GameNoticeService.java
@@ -6,9 +6,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.hamgame.hamgame.config.security.auth.UserPrincipal;
 import com.hamgame.hamgame.domain.Game;
 import com.hamgame.hamgame.domain.User;
 import com.hamgame.hamgame.dto.GameNoticeDto;
+import com.hamgame.hamgame.exception.CustomException;
+import com.hamgame.hamgame.exception.ErrorCode;
 import com.hamgame.hamgame.repository.GameNoticeRepository;
 import com.hamgame.hamgame.repository.UserRepository;
 
@@ -20,8 +23,9 @@ public class GameNoticeService {
 	private final GameNoticeRepository gameNoticeRepository;
 	private final UserRepository userRepository;
 
-	public Page<GameNoticeDto> getMyGameNoticeList(Pageable pageable) {
-		User user = userRepository.findById(1L).orElseThrow(RuntimeException::new); // 수정
+	public Page<GameNoticeDto> getMyGameNoticeList(Pageable pageable, UserPrincipal userPrincipal) {
+		User user = userRepository.findById(userPrincipal.getId())
+			.orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 		Set<Game> games = user.getGames();
 		return gameNoticeRepository.findByGameIn(games, pageable).map(GameNoticeDto::of);
 
