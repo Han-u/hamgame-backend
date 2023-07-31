dfdd0798c7ddc575469074b5c16d4154d4810f09
han <chos9715@naver.com>
2023-07-31 18:59:39 +0900

    [Feat] 즐겨찾는 게임 User 정보 추가

--- src/main/java/com/hamgame/hamgame/controller/FavoriteController.java
+++ src/main/java/com/hamgame/hamgame/controller/FavoriteController.java
@@ -1,6 +1,6 @@
 package com.hamgame.hamgame.controller;
 
-import java.util.Set;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hamgame.hamgame.domain.Game;
+import com.hamgame.hamgame.common.CurrentUser;
+import com.hamgame.hamgame.config.security.auth.UserPrincipal;
 import com.hamgame.hamgame.dto.AddFavGamesReq;
+import com.hamgame.hamgame.dto.GameDto;
 import com.hamgame.hamgame.dto.RemoveFavGamesReq;
 import com.hamgame.hamgame.dto.UpdateFavGamesReq;
 import com.hamgame.hamgame.service.FavoriteService;
@@ -25,23 +27,23 @@ public class FavoriteController {
 	private final FavoriteService favoriteService;
 
 	@GetMapping
-	public Set<Game> getFavoriteGameList() {
-		return favoriteService.getFavoriteGameList();
+	public List<GameDto> getFavoriteGameList(@CurrentUser UserPrincipal userPrincipal) {
+		return favoriteService.getFavoriteGameList(userPrincipal);
 	}
 
 	@PostMapping
-	public void addFavoriteGames(AddFavGamesReq requestDto) {
-		favoriteService.addGame(requestDto);
+	public void addFavoriteGames(AddFavGamesReq requestDto, @CurrentUser UserPrincipal userPrincipal) {
+		favoriteService.addGame(requestDto, userPrincipal);
 	}
 
 	@PutMapping
-	public void updateFavoriteGames(UpdateFavGamesReq requestDto) {
-		favoriteService.updateGames(requestDto);
+	public void updateFavoriteGames(UpdateFavGamesReq requestDto, @CurrentUser UserPrincipal userPrincipal) {
+		favoriteService.updateGames(requestDto, userPrincipal);
 	}
 
 	@DeleteMapping
-	public void removeGames(RemoveFavGamesReq requestDto) {
-		favoriteService.removeGame(requestDto);
+	public void removeGames(RemoveFavGamesReq requestDto, @CurrentUser UserPrincipal userPrincipal) {
+		favoriteService.removeGame(requestDto, userPrincipal);
 	}
 
 }
