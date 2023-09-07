5b1b33f53cb38001dfcd6558cbfc2d8656f6c9ff
han <chos9715@naver.com>
2023-09-08 03:28:22 +0900

    [Feat] Provider 추가

--- src/main/java/com/hamgame/hamgame/security/oauth/OAuth2UserInfoFactory.java
+++ src/main/java/com/hamgame/hamgame/security/oauth/OAuth2UserInfoFactory.java
@@ -2,16 +2,20 @@ package com.hamgame.hamgame.security.oauth;
 
 import java.util.Map;
 
+import com.hamgame.hamgame.security.oauth.provider.Google;
 import com.hamgame.hamgame.security.oauth.provider.Kakao;
 import com.hamgame.hamgame.domain.user.entity.Provider;
+import com.hamgame.hamgame.security.oauth.provider.Naver;
 
 public class OAuth2UserInfoFactory {
 	public static OAuth2UserInfo getOAuth2UserInfo(Provider provider, Map<String, Object> attributes) {
 		if (provider == Provider.KAKAO) {
 			return new Kakao(attributes);
+		} else if (provider == Provider.NAVER){
+			return new Naver(attributes);
+		} else if (provider == Provider.GOOGLE){
+			return new Google(attributes);
 		}
-
-		// 나머지 나중에
 		throw new IllegalArgumentException("Invalid Provider");
 	}
 }
